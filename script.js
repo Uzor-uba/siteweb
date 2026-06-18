f193d503310b57923607c769eddf7da911673729
Uzor-uba <uzorubaagwata.com>
2026-06-18 15:23:40 +0200

    add js polyfill

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    const menuOpenButton = document.querySelector('button[command="show-modal"][commandfor="mobile-menu"]');
-    const menuCloseButton = document.querySelector('button[command="close"][commandfor="mobile-menu"]');
-    const mobileMenu = document.getElementById("mobile-menu");
-
-    menuOpenButton.addEventListener("click", function () {
-        mobileMenu.setAttribute("open");
-    });
-
-    menuCloseButton.addEventListener("click", function () {
-        mobileMenu.removeAttribute("open");
-    });
+    // TODO
 
 });
